f7eeee09e133447f3060e2cc780edc3d06a72866
Alexander <x48j35@gmail.com>
2015-12-04 16:10:37 +0300

    Add default port

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ var app = express();
 app.set('view engine', 'jade');
 app.use(express.static('styles'));
 app.use(express.static('images'));
-app.set('port', (process.env.PORT || 300));
 
 
 app.get('/*', function(req, res) {
   res.render('index',{ title: 'Hey', message: 'Hello there!'});
 })
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(3000, function() {
+  console.log('Hi there!');
 });
