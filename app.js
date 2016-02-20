7d3c6bd618a781804bdf1ae51d57523888bdd81d
Alexander <x48j35@gmail.com>
2016-02-20 23:14:51 +0300

    Add openshift configuration

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ app.use(express.static('styles'));
 app.use(express.static('images'));
 app.use(express.static('js'));
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 
 app.get('/*', function(req, res) {
   res.render('index',{ title: 'Hey', message: 'Hello there!'});
 })
 
-app.listen(3000, function() {
+app.listen(server_port, server_ip_address, function() {
   console.log('Hi there!');
 });
